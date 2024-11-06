6440b9e96c53683928c4b88c26e23792fd0873b7
yyan0234 <yyan0234@student.monash.edu>
2024-11-06 23:49:55 +1100

    Remove all files

--- team005-app_fit3047-new_dev/templates/Payments/secrets.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Keep your Stripe API key protected by including it as an environment variable
-// or in a private script that does not publicly expose the source code.
-
-// This is your test secret API key.
-$stripeSecretKey = 'sk_test_51N5C66FipqYPgRnRSqtWVuhYLv3Mlxyz2oRz8FVLqUalhvlXZblpMw2Qa2OSHbs7YCt3A3jDVsfKvGcpLglV8aEq00V1KjZPXQ';
